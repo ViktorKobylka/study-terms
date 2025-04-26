52b1e0ff25ce7dc3563b3a2087ffaceb72de74cd
ViktorKobylka <g00425163@atu.ie>
2025-04-26 20:32:43 +0100

    use standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, IonicModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

1f98f41d8ceb82596236effa7d5d2ada7e8be908
ViktorKobylka <g00425163@atu.ie>
2025-04-27 18:44:43 +0100

    added setting page structure and clear all data from storage, changed terms page structure

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { IonicModule, ToastController } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-settings',
+  selector: 'app-settings-page',
   standalone: true,
+  imports: [IonicModule, CommonModule],
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss'],
 })
-export class SettingsComponent  implements OnInit {
+export class SettingsComponent {
 
-  constructor() { }
+  constructor(private toastController: ToastController) {}
 
-  ngOnInit() {}
-
+  async clearLocalStorage() {
+    localStorage.clear();
+    console.log('Local storage cleared.');
+    const toast = await this.toastController.create({
+      message: 'Local storage cleared successfully!',
+      duration: 1000,
+      position: 'bottom',
+      color: 'success',
+    });
+    await toast.present();
+  }
 }

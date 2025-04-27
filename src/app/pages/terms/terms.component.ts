a4742f43d6e1abdaede80d27a388b3f35f3ca743
ViktorKobylka <g00425163@atu.ie>
2025-04-27 02:35:59 +0100

    created structure of terms page, added load and delete methods

--- src/app/pages/terms/terms.component.ts
+++ src/app/pages/terms/terms.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-terms',
   standalone: true,
+  imports: [CommonModule, IonicModule],
   templateUrl: './terms.component.html',
   styleUrls: ['./terms.component.scss'],
 })
-export class TermsComponent  implements OnInit {
+export class TermsComponent implements OnInit {
+  savedResults: any[] = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.loadSavedResults();
+  }
 
+  loadSavedResults() {
+    const saved = localStorage.getItem('savedResults');
+    this.savedResults = saved ? JSON.parse(saved) : [];
+  }
+
+  deleteResult(index: number) {
+    this.savedResults.splice(index, 1); // remove item at index
+    localStorage.setItem('savedResults', JSON.stringify(this.savedResults));
+  }
+
+  refreshPage() {
+    this.loadSavedResults();
+  }
+  
 }

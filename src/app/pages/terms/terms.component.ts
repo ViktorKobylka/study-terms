a04820bde200f01203b3511441e8d6c5e746836f
ViktorKobylka <g00425163@atu.ie>
2025-04-27 19:24:59 +0100

    added css and drag and handle functionality for terms page

--- src/app/pages/terms/terms.component.ts
+++ src/app/pages/terms/terms.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
+import { ItemReorderEventDetail } from '@ionic/angular';
 
 @Component({
   selector: 'app-terms',
@@ -23,13 +24,28 @@ export class TermsComponent implements OnInit {
     this.savedResults = saved ? JSON.parse(saved) : [];
   }
 
+  handleReorder(event: CustomEvent<ItemReorderEventDetail>) {
+    this.loadSavedResults();
+
+    const from = event.detail.from;
+    const to = event.detail.to;
+
+    const movedItem = this.savedResults.splice(from, 1)[0];
+    this.savedResults.splice(to, 0, movedItem);
+
+    localStorage.setItem('savedResults', JSON.stringify(this.savedResults));
+
+    event.detail.complete();
+  }
+
   deleteResult(index: number) {
-    this.savedResults.splice(index, 1); // remove item at index
+    this.loadSavedResults();
+    
+    this.savedResults.splice(index, 1);
     localStorage.setItem('savedResults', JSON.stringify(this.savedResults));
   }
 
   refreshPage() {
     this.loadSavedResults();
   }
-  
 }
